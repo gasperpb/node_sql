4c4a3f549ecdc08d7befb3482e19f6eee1d31feb
gasperpb <achillespb@hotmail.com>
2024-06-11 17:10:59 -0300

    deletando usuarios

---
+++ README.md
@@ -0,0 +1,12 @@
+# Estudo de conexão com o banco mysql
+ Crie o banco em msyql com a tabela usuarios com os campos id, nome, email.
+ Comando para cria o server npm init
+
+
+
+
+
+
+Tue, Jun 11, 2024  3:31:28 PM
+cria uma forma de inserir usuarios
+Aprendi a delelar ussuarios o proximo passo e editar e depois levar para react.js
--- app.js
+++ app.js
@@ -12,22 +12,29 @@ connection.connect(function(err){
     console.log('conectou!');
 })
 
-const nomes = [
-    'Ana', 'Beatriz', 'Carlos', 'Daniela', 'Eduardo', 'Fernanda', 'Gabriel', 'Helena', 'Igor',
-    'Joana', 'Kleber', 'Lívia', 'Marcelo', 'Nívea', 'Otávio', 'Patrícia', 'Quirino', 'Rafaela',
-    'Sandro', 'Tânia', 'Ulisses', 'Viviane', 'William', 'Xuxa', 'Yasmin', 'Zilda', 'Alberto',
-    'Bianca', 'Cecília', 'Dante'
-  ];
-
-  for (let i = 0; i < nomes.length; i++) {
-    const email = `${nomes[i].toLowerCase()}@exemplo.com`;
-    connection.query('INSERT INTO usuarios (nome, email) VALUES (?, ?)', [nomes[i], email], (error, results, fields) => {
-      if (error) throw error;
-      // Outras ações após a inserção, se necessário
-    });
-  }
-
-
+// const nomes = [
+//     'Ana', 'Beatriz', 'Carlos', 'Daniela', 'Eduardo', 'Fernanda', 'Gabriel', 'Helena', 'Igor',
+//     'Joana', 'Kleber', 'Lívia', 'Marcelo', 'Nívea', 'Otávio', 'Patrícia', 'Quirino', 'Rafaela',
+//     'Sandro', 'Tânia', 'Ulisses', 'Viviane', 'William', 'Xuxa', 'Yasmin', 'Zilda', 'Alberto',
+//     'Bianca', 'Cecília', 'Dante','Leandro', 'Mirella', 'Natan', 'Olívia', 'Paulo', 'Quezia', 'Ricardo', 'Sônia', 'Tiago',
+//   'Ursula', 'Valdir', 'Wanessa', 'Xavier', 'Yuri', 'Zara', 'Aline', 'Bruno', 'Camila',
+//   'Diego', 'Elisa', 'Fábio', 'Gisele', 'Hugo', 'Íris', 'Júlio', 'Karina', 'Luciano',
+//   'Mônica', 'Norberto', 'Oscar'
+//   ];
+//       // inserir os nomes no banco de dados
+//   for (let i = 0; i < nomes.length; i++) {
+//     const email = `${nomes[i].toLowerCase()}@exemplo.com`;
+//     connection.query('INSERT INTO usuarios (nome, email) VALUES (?, ?)', [nomes[i], email], (error, results, fields) => {
+//       if (error) throw error;
+//       // Outras aç2ões após a inserção, se necessário
+//     });
+//   }
+// Consultar os registros da tabela
 connection.query('SELECT * FROM usuarios', function(err, results, fields){      
     console.log(results);
 });
+
+//Deletar registros da tabela
+// connection.query('DELETE FROM usuarios WHERE id = 1', [1], function(err, results, fields){
+//     console.log(results);
+// });
